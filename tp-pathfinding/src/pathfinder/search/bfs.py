0297d941249783c75bdf070049fb2d86dc8fb7b1
Ericagretel <erica.hinrichsen@gmail.com>
2025-03-27 20:01:51 -0300

    bfs

--- tp-pathfinding/src/pathfinder/search/bfs.py
+++ tp-pathfinding/src/pathfinder/search/bfs.py
@@ -23,5 +23,33 @@ class BreadthFirstSearch:
         
         # Add the node to the explored dictionary
         explored[node.state] = True
+        # aplicamos test objetivo
+        if node.state == grid.end:
+            return Solution(node, explored)
+        
+        #Inicializamos la frontera, en este caso es una Cola
+        frontier=QueueFrontier()
+        frontier.add(node)
+
+        while True:
+            #falla si frontera vacía
+            if frontier.is_empty():
+                return NoSolution(explored)
+            #sacar nodo de frontera
+            node=frontier.remove()
+            print(node.state)
+            successors=grid.get_neighbours(node.state)
+            
+            for a in successors:
+                new_pos=successors[a]
+                if new_pos not in explored:
+                    n1=Node("",new_pos,grid.get_cost(new_pos)+ node.cost, node,a)
+                    if n1.state == grid.end:
+                        return Solution(n1,explored)
+                    explored[new_pos]=True
+                    frontier.add(n1)
+
+
+
         
         return NoSolution(explored)
